ec299d2ad8a9c7b4766a1e0e3e7a338bb5a70659
Kizito-Alberrt <wagabi98@gmail.com>
2020-12-22 11:53:27 +0300

    added js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-  $(".icon-3").click(function(){
-  alert("mmmmmd")
-});
+$(document).ready(function() {
+  $(".atv").click(function() {
+    $(".pr").slideToggle();
+  });
+  $(".atv-2").click(function(){
+    $(".pr-2").slideToggle();
+  })
+  $(".atv-3").click(function(){
+    $(".pr-3").slideToggle();
+  })
 });
